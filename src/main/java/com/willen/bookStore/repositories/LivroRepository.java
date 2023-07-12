0f2ef71e8446f983dbfc67b81da293cd43125c16
willen <willen.sesantos@gmail.com>
2023-07-12 20:52:55 -0300

    chore: Implementa repositories

---
+++ src/main/java/com/willen/bookStore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.willen.bookStore.repositories;
+
+import com.willen.bookStore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
