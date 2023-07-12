8c27ea8438e315d05b066820b47dd13e768e15fc
willen <willen.sesantos@gmail.com>
2023-07-12 20:12:48 -0300

    chore: Finaliza classes de dominio

--- src/main/java/com/willen/bookStore/domain/Categoria.java
+++ src/main/java/com/willen/bookStore/domain/Categoria.java
@@ -1,14 +1,21 @@
 package com.willen.bookStore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 
     public Categoria() {
--- src/main/java/com/willen/bookStore/domain/Livro.java
+++ src/main/java/com/willen/bookStore/domain/Livro.java
@@ -1,14 +1,21 @@
 package com.willen.bookStore.domain;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
